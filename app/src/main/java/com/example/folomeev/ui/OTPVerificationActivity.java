f1732846cec2b320af667dffeee87beaa7072c62
Sheba <Provork2@gmail.com>
2026-01-16 16:14:42 +0300

    0.9.0

--- app/src/main/java/com/example/folomeev/ui/OTPVerificationActivity.java
+++ app/src/main/java/com/example/folomeev/ui/OTPVerificationActivity.java
@@ -1,26 +1,145 @@
 package com.example.folomeev.ui;
 
+import static com.example.folomeev.utils.Utils.APIKEY;
+import static com.example.folomeev.utils.Utils.BASE_URL;
+import static com.example.folomeev.utils.Utils.TOKEN;
+
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.res.ResourcesCompat;
 
+import com.chaos.view.PinView;
 import com.example.folomeev.R;
+import com.example.folomeev.controller.API;
+import com.example.folomeev.data.ChangePasswordToken;
+import com.example.folomeev.data.Email;
+import com.example.folomeev.data.ResponseUser;
+import com.google.android.material.button.MaterialButton;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class OTPVerificationActivity extends AppCompatActivity {
+
+    TextView resendText;
+    boolean runningTimer;
+    MaterialButton NewPassButton;
+    CountDownTimer resetCountDownTimer;
+
+    PinView pinView;
+    Retrofit retrofit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_otpverification);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_otp_verification);
+
+        NewPassButton = findViewById(R.id.SetNewPass);
+        resendText = findViewById(R.id.resendAfter);
+        pinView = findViewById(R.id.pinView);
+        String emailPass = getIntent().getStringExtra("email");
+        String emailRepeat = getIntent().getStringExtra("emailRepeat");
+        Email email = new Email(emailRepeat);
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        API api = retrofit.create(API.class);
+        resetCountDownTimer = new CountDownTimer(59000, 1000) {
+
+
+            @Override
+            public void onTick(long l) { resendText.setText("resend after 0:" + (l / 1000 + 1)); }
+
+
+            @Override
+            public void onFinish() {
+                resendText.setText("resend");
+                resendText.setTextColor(Color.parseColor("#006CEC"));
+                runningTimer = false;
+                resendText.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        ResetTimer();
+                        Call<Void> call = api.sendCode(APIKEY, email);
+                        call.enqueue(new Callback<Void>() {
+                            @Override
+                            public void onResponse(Call<Void> call, Response<Void> response) {
+                                if (response.isSuccessful()) {
+                                    Toast.makeText(OTPVerificationActivity.this, "Code Resended", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Call<Void> call, Throwable t) {
+                                Toast.makeText(OTPVerificationActivity.this, t.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+            }
+        }.start();
+
+        NewPassButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (pinView.getText().toString().length() == 6) {
+                    ChangePasswordToken changePasswordToken =
+                            new ChangePasswordToken(
+                                    "email",
+                                    emailPass,
+                                    pinView.getText().toString());
+                    Call<ResponseUser> call = api.verifyCode(APIKEY, changePasswordToken);
+                    call.enqueue(new Callback<ResponseUser>() {
+                        @Override
+                        public void onResponse(Call<ResponseUser> call, Response<ResponseUser> response) {
+                            if (response.isSuccessful()) {
+                                if (response.body() != null) {
+                                    TOKEN = response.body().accessToken;
+                                    Toast.makeText(OTPVerificationActivity.this, TOKEN + "", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(OTPVerificationActivity.this, NewPasswordActivity.class);
+
+                                    intent.putExtra("passedEmail", emailPass);
+                                    intent.putExtra("newToken", TOKEN);
+                                    startActivity(intent);
+
+                                }
+                            } else {
+                                pinView.setLineColor(ResourcesCompat.getColor(getResources(), R.color.red, getTheme()));
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<ResponseUser> call, Throwable throwable) {
+
+                        }
+                    });
+                } else {
+                    Toast.makeText(OTPVerificationActivity.this, "Введите текст", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
+    }
+
+
+    private void ResetTimer() {
+        if (runningTimer == false) {
+            runningTimer = true;
+            resendText.setTextColor(Color.parseColor("#A7A7A7"));
+            resetCountDownTimer.start();
+        }
     }
 }
--- app/src/main/java/com/example/folomeev/ui/SignUpActivity.java
+++ app/src/main/java/com/example/folomeev/ui/SignUpActivity.java
@@ -1,26 +1,87 @@
 package com.example.folomeev.ui;
 
-import android.os.Bundle;
+import static com.example.folomeev.utils.Utils.APIKEY;
+import static com.example.folomeev.utils.Utils.BASE_URL;
 
-import androidx.activity.EdgeToEdge;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.folomeev.R;
+import com.example.folomeev.controller.API;
+import com.example.folomeev.data.User;
+import com.example.folomeev.utils.Utils;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
 
-public class SignUpActivity extends AppCompatActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class SignUpActivity extends AppCompatActivity { //активность обработки запроса на регистрацию пользователя
+
+    TextInputLayout name, phone, email, pswrd, pswrdConfirm;
+    MaterialButton button;
+    Retrofit retrofit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        name = findViewById(R.id.fullname);
+        phone = findViewById(R.id.phoneNumber);
+        email = findViewById(R.id.emailAddress);
+        pswrd = findViewById(R.id.passwordTextInputLayout);
+        pswrdConfirm = findViewById(R.id.confirmPasswordTextInputLayout);
+        button = findViewById(R.id.button_signup);
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        API api = retrofit.create(API.class);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            //регистрация пользователя на сервере
+            @Override
+            public void onClick(View view) {
+                //создание объекта пользователь
+                Utils.user = new User(
+                        String.valueOf(name.getEditText().getText()),
+                        String.valueOf(phone.getEditText().getText()),
+                        String.valueOf(email.getEditText().getText()),
+                        String.valueOf(password.getEditText().getText())
+                );
+                //запрос к базе данных на регистрацию пользователя
+                Call<Void> call = api.registration(APIKEY, Utils.user);
+                call.enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        if (response.isSuccessful()) {
+                            Toast.makeText(SignUpActivity.this, "sign up ok", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(SignUpActivity.this, LogInActivity.class));
+                        } else {
+                            Toast.makeText(SignUpActivity.this, response.code() + "", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        Toast.makeText(SignUpActivity.this, t.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
         });
+    }
+
+    public void signin(View view) {
+        startActivity(new Intent(SignUpActivity.this, LogInActivity.class));
     }
 }
